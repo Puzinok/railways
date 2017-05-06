a53924fccc80d8f6aef046bf8b1718f49949713f
Pavel Puzin <puzinok@gmail.com>
2017-05-06 20:42:32 +0300

    add Routes resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505072947) do
+ActiveRecord::Schema.define(version: 20170506163300) do
 
   create_table "railways_stations", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "trains", force: :cascade do |t|
