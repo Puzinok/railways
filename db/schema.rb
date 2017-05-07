721366b7df2a12c7961f8420da24abf603ae54e5
Pavel Puzin <puzinok@gmail.com>
2017-05-07 23:46:39 +0300

    Change schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507202907) do
+ActiveRecord::Schema.define(version: 20170507203818) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "name"
@@ -18,14 +18,8 @@ ActiveRecord::Schema.define(version: 20170507202907) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "railways_stations", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "railways_stations_routes", force: :cascade do |t|
-    t.integer "railways_station_id"
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id"
     t.integer "route_id"
   end
 
