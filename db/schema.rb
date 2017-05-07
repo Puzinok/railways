e2b9c4921fe45e01a005bb03686d3997f694b859
Pavel Puzin <puzinok@gmail.com>
2017-05-07 23:36:31 +0300

    add RailwayStation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506163300) do
+ActiveRecord::Schema.define(version: 20170507202907) do
+
+  create_table "railway_stations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "railways_stations", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "railways_stations_routes", force: :cascade do |t|
+    t.integer "railways_station_id"
+    t.integer "route_id"
   end
 
   create_table "routes", force: :cascade do |t|
