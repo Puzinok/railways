87913a726079bdaf534c675b9a2dec73cc8eca2a
Pavel Puzin <puzinok@gmail.com>
2017-05-10 03:13:23 +0300

    Finished Task 14

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -43,6 +43,6 @@ class TrainsController < ApplicationController
     end
 
     def train_params
-      params.require(:train).permit(:number)
+      params.require(:train).permit(:number, :route_id)
     end
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,8 @@
 class Train < ApplicationRecord
   has_many :tickets
   
-  belongs_to :route
-  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :route, optional: true
+  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
+
+  validates :number, presence: true
 end
