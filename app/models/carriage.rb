09bcbff31ccd6bc7ed0a01959b18d9c4295e0896
Pavel Puzin <puzinok@gmail.com>
2017-05-14 23:09:21 +0300

    Add serial number to carriages

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,6 +1,8 @@
 class Carriage < ApplicationRecord
   belongs_to :train, optional: true
 
+  before_validation :set_number
+
   validates :upper_seat,
             :bottom_seat,
             :side_upper_seat,
@@ -8,4 +10,16 @@ class Carriage < ApplicationRecord
             :sitting_seat,
             :number, numericality: { allow_nil: true, only_integer: true,
                                      greater_than_or_equal_to: 0 }
+  
+  validates :number, uniqueness:{ scope: :train_id }
+  
+  private
+
+  def set_number
+    if self.train.carriages.first.nil?
+      self.number = 1
+    else
+      self.number = self.train.carriages.last.number + 1
+    end
+  end
 end
