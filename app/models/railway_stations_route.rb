760cd55a238ee6af3b339c506a6552cd6381e014
Pavel Puzin <puzinok@gmail.com>
2017-05-16 16:12:28 +0300

    add 'update position' station in route

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -2,7 +2,5 @@ class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
 
-  default_scope -> { order(:position) }
-
   validates :railway_station_id, uniqueness: { scope: :route_id }
 end
