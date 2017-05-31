41983f4eb4f4fadeb4741c658b1f853efe04ab01
Pavel Puzin <puzinok@gmail.com>
2017-05-31 21:58:57 +0300

    fix mailer bugs

--- app/mailers/tickets_mailer.rb
+++ app/mailers/tickets_mailer.rb
@@ -3,13 +3,13 @@ class TicketsMailer < ApplicationMailer
     @user = user
     @ticket = ticket
     
-    mail(to: user.email, subject: t('common.your_buy_ticket', route: ticket.route.name))
+    mail(to: user.email, subject: t('common.your_ticket', route: ticket.route_name))
   end
 
   def cancel_ticket(user, ticket)
     @user = user
     @ticket = ticket
     
-    mail(to: user.email, subject: t('common.cancellation_your_ticket'))
+    mail(to: user.email, subject: t('common.cancellation_your_ticket', route: ticket.route_name))
   end
 end
