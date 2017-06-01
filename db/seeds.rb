35949767794036a58c1b2f3eacecb183665f8a5a
Pavel Puzin <puzinok@gmail.com>
2017-06-01 16:30:32 +0300

    refactoring seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,36 +7,56 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 stations = %w[Москва Воронеж Тамбов Ярославль Казань Мичуринск Екатеринбург Тольятти Курск Мурманск]
 
-stations.each do |title|
-  RailwayStation.create(name: title.to_s)
-end
-
-all_stations = RailwayStation.all
-
-5.times do
-  random_stations = all_stations.sample(rand(2..all_stations.count))
-  new_route = Route.new
-  random_stations.each do |station|
-    new_route.railway_stations << station
+def create_stations(stations)
+  stations.each do |title|
+    RailwayStation.create(name: title.to_s)
   end
-  new_route.save!
 end
 
-Route.all.each do |route|
-  route.railway_stations.each.with_index(1) do |station, position|
-    station.update_position(route, position)
-    station.set_time(route, Time.at(rand(24.hours)), Time.at(rand(24.hours)))
+def create_routes(amount)
+  stations = RailwayStation.all
+  amount.times do
+    random_stations = stations.sample(rand(2..stations.count))
+    new_route = Route.new
+
+    random_stations.each do |station|
+      new_route.railway_stations << station
+    end
+    new_route.save!
   end
-  train_number = "#{('A'..'Z').to_a.shuffle[0,2].join}"+"-"+"#{rand(000..1000)}"
-  Train.create(number: train_number, route: route)
 end
+
+def random_time
+  min_time = Time.zone.now
+  max_time = Time.zone.now + 3.months
+  rand(min_time..max_time)
+end
+
+def random_train_number
+  ('A'..'Z').to_a.sample(2).join.to_s + '-' + rand(100..999).to_s
+end
+
+def create_trains
+  Route.all.each do |route|
+    route.railway_stations.each.with_index(1) do |station, position|
+      station.update_position(route, position)
+      station.set_time(route, random_time, random_time)
+    end
+
+    Train.create(number: random_train_number, route: route)
+  end
+end
+
+create_stations(stations)
+create_routes(10)
+create_trains
 
 admin = User.new(
   name: 'Pavel',
   surname: 'Puzin',
   email: 'admin@example.com',
   admin: true,
-  password: '123456', 
+  password: '123456',
   password_confirmation: '123456'
 )
 admin.skip_confirmation!
