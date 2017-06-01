b04ee394bf1ae0a2d650e2b48b2008f40f7758b1
Pavel Puzin <puzinok@gmail.com>
2017-06-01 13:36:19 +0300

    add, mailer host in production, fix style

--- app/mailers/tickets_mailer.rb
+++ app/mailers/tickets_mailer.rb
@@ -2,14 +2,14 @@ class TicketsMailer < ApplicationMailer
   def buy_ticket(user, ticket)
     @user = user
     @ticket = ticket
-    
+
     mail(to: user.email, subject: t('common.your_ticket', route: ticket.route_name))
   end
 
   def cancel_ticket(user, ticket)
     @user = user
     @ticket = ticket
-    
+
     mail(to: user.email, subject: t('common.cancellation_your_ticket', route: ticket.route_name))
   end
 end
